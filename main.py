60b3f35e3c041f4cc1b067ff6c03bf7efefa71e3
seorakwon <seo-rakwon@kpmg.es>
2022-08-01 14:50:37 +0200

    main y config actualizado

--- main.py
+++ main.py
@@ -15,30 +15,32 @@ def check_input(input_file):
     """
     Checks if input file has header and retrieves the delimiter
     """
-    with open(input_file, 'r') as csvfile:
-        sample = csvfile.read(64)
-        has_header = csv.Sniffer().has_header(sample)#esto una funcion si tiene cabecera
+    #try except no encuentra delimitador
+    try:
+        with open(input_file, 'r') as csvfile:
+            sample = csvfile.read(64)
+            has_header = csv.Sniffer().has_header(sample)
+            dialect = csv.Sniffer().sniff(sample)
+            print('Header and delimiter detected')
 
-        delimiter = csv.Sniffer().sniff(sample)#esto una funcion encontrar delimitador
-        #try except no encuentra delimitador
+    except:
+        pass
         
-    
-    return has_header, delimiter
+    return has_header, dialect.delimiter
     
 
 def read_input(input_file):
     #TODO por ahora lo dejamos en csv que es con lo que estamos trabajando, ampliar a excel?
     #fichero config, separador y tipo de fichero .csv .xls .xlsx (llamar funcion para leer excel, y ver si 
     # sniffer es capaz de extraer separador)
-    has_header, delimiter = check_input(config['read'][input_file])
-
+    has_header, delimiter = check_input(input_file)
+    
     if has_header:
         try:
             df = pd.read_csv(input_file, sep=delimiter)
-            print('1111')
+            print('File read with detected delimiter')
         except(FileNotFoundError, PermissionError) as f:
             print(f)
-
         return df
     else:
         print('Please provide input with headers')
@@ -46,5 +48,6 @@ def read_input(input_file):
     
 
 if __name__ == "__main__":
-    print(config.sections())
-    #read_input(config['input_info']['input_file'])
+    input_file = config['input_info']['input_file']
+    read_input(input_file)
+    
