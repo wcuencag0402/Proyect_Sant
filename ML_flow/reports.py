a74f4eb2d66c6cfd9d907a35b1312b777be5cd34
wcuencag0402 <wcuenca@kpmg.es>
2022-08-02 17:03:06 +0200

    esqueleto de la clase report

---
+++ ML_flow/reports.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+class report_quality:
+    def __init__:
+        self.out_path = ......
+    
+    def save_report(self, df):
+        '''
+        def: Save data report in format excel
+        param: datframes
+        return: None
+        '''
+        pass
+
+    def calculate_statistics(self, df):
+        '''
+        def: Calculate statistics form all columns in dataframes
+        param: datframes
+        return: dataframes
+        '''
+        pass
+
+    def run_report(self, df):
+        '''
+        def: Run data quality report
+        params: dataframe
+        return: None
+        ''' 
+        pass
+
+if __name__ == "__main__":
+    # Realizar pruebas del proceso con distintos 
+    # valores de las columnas
+
+
